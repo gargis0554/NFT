455722cc2d3c85ae547e155c251011214c78bed2
Gargishukla <shuklagargi455@gmail.com>
2025-04-14 19:53:35 +0530

    commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  // networks: {
+  //   polygonMumbai: {
+  //     url: "https://rpc-mumbai.maticvigil.com",
+  //     accounts: ["YOUR_PRIVATE_KEY"]
+  //   },
+  //   polygonMainnet: {
+  //     url: "https://polygon-rpc.com",
+  //     accounts: ["YOUR_PRIVATE_KEY"]
+  //   }
+  // }
+};
